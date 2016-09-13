12a2708fc29326c17d91551a03bcb6fd381c5742
Juan Blanco <juanfranblanco@hotmail.com>
2016-09-13 18:48:21 +0100

    initial commit

---
+++ Origami/Assets/Scripts/BlockNumber.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+#if WINDOWS_UWP 
+using System.Collections;
+using Nethereum.Web3;
+#endif
+public class BlockNumber : MonoBehaviour {
+
+
+    private static DateTime LastTimeAccessed; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+#if WINDOWS_UWP 
+        if (LastTimeAccessed.AddSeconds(5) < DateTime.Now)
+        {
+            try
+            {
+                var web3 = new Web3("https://morden.infura.io/aEcNY6wGN4KuEpoXQRxZ");
+                var blockNumber = web3.Eth.Blocks.GetBlockNumber.SendRequestAsync().Result;
+                   GetComponent<TextMesh>().text = "Last block:" + blockNumber.Value.ToString();
+                LastTimeAccessed = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                GetComponent<TextMesh>().text = ex.Message + ex.StackTrace;
+            }
+        }
+#endif
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+#Hololens sample
+
+Very simple sample to demonstrate integration with Unity3d, UWP and Hololens.
+
+Unity3d does not support (yet) the latest version of .net for scripting, but this is not an issue with UWP.
+
+This project demonstrates how to enable this by using the [Origami sample](https://developer.microsoft.com/en-us/windows/holographic/holograms_101e) from the 101e on Holograms by Microsoft,
+and adding an script in which we will get the latest blockNumber.
+
+##Video sample
+This video provides a quick overview of the structure, #pragmas and dlls necessary on the plugins folder.
+
+https://www.youtube.com/watch?v=2MXAWHTRw7E
